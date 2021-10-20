10007f6fa901e7b6b08f67d497bb410a69dab32a
s11193950 <70602031+s11193950@users.noreply.github.com>
2021-10-20 09:38:44 -0400

    v0.5
    added the scanner

---
+++ DamageDealt.java
@@ -0,0 +1,14 @@
+// <jarodkyles>, AP CSA Unit 1 Challenge, Andrew Jackson High School, <10/20/21>, <9:02>, v0.5
+
+import java.util.Scanner;
+    
+ public class damageDealt{
+     public static void main(string{}args)[
+            Scanner myScanner = new Scanner(System.in);
+
+           System.out.println("please Enteryour username");
+
+           String userName= myScanner.nextLine();
+
+          System.out.println("your username is:")
+
